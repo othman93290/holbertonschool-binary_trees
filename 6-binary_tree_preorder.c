2e5edcf9468c66a45ed9b16a0c2f59a828e9077c
othman93290 <7566@holbertonstudents.com>
2024-01-09 06:22:36 -0800

    betty

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,7 +5,7 @@
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
- * Description: This function traverses a binary tree using pre-order traversal.
+ * Description: This function traverses a binary tree using pre-order traversal
  * It starts from the root node and applies the given function to each node,
  * passing the value in the node as a parameter to the function.
  * If tree or func is NULL, do nothing.
@@ -19,4 +19,3 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
-
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ * binary_tree_postorder - Post-order traversal of a binary tree
  * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
+ * @func: Pointer to a function to call for each node's value
  *
- * Description: This function traverses a binary tree using post-order traversal.
- * It starts from the root node and applies the given function to each node's value,
- * passing the value in the node as a parameter to the function.
- * If tree or func is NULL, do nothing.
+ * Description: This function performs a post-order traversal of a binary tree,
+ * starting from the root node. It applies the given function to each node's
+ * value, passing the node's value as a parameter to the function.
+ * If tree or func is NULL, no action is taken.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
